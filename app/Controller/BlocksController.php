6da912210adccf03550b8005a84e39f937fee256
wakiyamaP <wakiyamap@gmail.com>
2015-05-29 09:00:13 +0900

    add等の不具合修正

--- app/Controller/BlocksController.php
+++ app/Controller/BlocksController.php
@@ -9,13 +9,28 @@ class BlocksController extends AppController {
       
     }
 
+    public function detail() {
+      //$autoLayout = false;
+      $dbBlockData = $this->Block->find('all',array(
+        'order' => array('Block.height' => 'desc'),
+        'limit' => 6, 
+      ));
+      $dbBlockData = array_reverse($dbBlockData);
+      $this->viewClass = 'Json';
+      $this->set(compact('dbBlockData'));
+      $this->set('_serialize','dbBlockData');
+    }
+
     public function getblockdata() {
       $autoLayout = false;
       $dbBlockData = $this->Block->find('all',array(
         'order' => array('Block.height' => 'desc'),
         'limit' => 6, 
       ));
-      $this->set("dbBlockData",$dbBlockData);
+      $dbBlockData = array_reverse($dbBlockData);
+      $this->viewClass = 'Json';
+      $this->set(compact('dbBlockData'));
+      $this->set('_serialize','dbBlockData');
 
     }
 
@@ -30,19 +45,42 @@ class BlocksController extends AppController {
         if($nextBlockData = @file_get_contents("http://160.16.76.211:3000/api/block/".$BlockData['nextblockhash'])){
           $nextBlockData = json_decode($nextBlockData,true);
 
-          $new_blockData = array(
-            'Block' => array(
-              'height' => $nextBlockData['height'],
-              'block_hash' => $nextBlockData['hash'],
-              'difficulty' => $nextBlockData['difficulty'],
-              'size' => $nextBlockData['size'],
-              'tx_count' => count($nextBlockData['tx']),
-              'time' => date('Y-m-d H:i:s',$nextBlockData['time']),
-            )
-          );
-          $this->Block->save($new_blockData) or die('{"error":"failed_save_address"}');
-
-          $this->set("jsonData",$nextBlockData);
+          if(array_key_exists('nextblockhash',$BlockData)){
+            $new_blockData = array(
+              'Block' => array(
+                'height' => $nextBlockData['height'],
+                'block_hash' => $nextBlockData['hash'],
+                'difficulty' => $nextBlockData['difficulty'],
+                'size' => $nextBlockData['size'],
+                'tx_count' => count($nextBlockData['tx']),
+                'time' => date('Y-m-d H:i:s',$nextBlockData['time']),
+              )
+            );
+            $this->set("jsonData",$nextBlockData);
+            $this->Block->save($new_blockData) or die('{"error":"failed_save_address"}');
+          } elseif($BlockData['isMainChain'] <> 1){
+            if($previousBlockData = @file_get_contents("http://160.16.76.211:3000/api/block/".$BlockData['previousblockhash'])){
+              $previousBlockData = json_decode($previousBlockData,true);
+              if($rightBlockData = @file_get_contents("http://160.16.76.211:3000/api/block/".$previousBlockData['nextblockhash'])){
+                $rightBlockData = json_decode($rightBlockData,true);
+                $right_blockData = array(
+                  'Block' => array(
+                    'id' => $dbBlockData['Block']['id'],
+                    'height' => $rightBlockData['height'],
+                    'block_hash' => $rightBlockData['hash'],
+                    'difficulty' => $rightBlockData['difficulty'],
+                    'size' => $rightBlockData['size'],
+                    'tx_count' => count($rightBlockData['tx']),
+                    'time' => date('Y-m-d H:i:s',$rightBlockData['time']),
+                  )
+                );
+                $this->set("jsonData",$rightBlockData);
+                $this->Block->save($right_blockData) or die('{"error":"failed_save_address"}');
+              }
+            }
+          } else {
+            $this->set("jsonData","error4");
+          }
         } else {
           $this->set("jsonData","error2");
         }
--- app/webroot/js/block.js
+++ app/webroot/js/block.js
@@ -1,5 +1,31 @@
 $(function(){
     setInterval(function(){
-        $("#blockdata").load("/blocks/getblockdata");
-    },5000);
+			$.getJSON("/blocks/getblockdata", function(Data){
+				var removeBlock = Data[5].Block.height -6;
+				if(document.getElementById('block'+Data[5].Block.height) != null){}else{
+					$(Data).each(function(){
+						if(document.getElementById('block'+this.Block.height) != null){
+							return true;
+						}else{
+							$(
+								'<div id="block'+this.Block.height+'">'+this.Block.time+'　'+
+								'<a href="/blocks/detail/' + this.Block.height + '">' + this.Block.height + '</a>'+
+								'　'+this.Block.tx_count+
+								'　'+this.Block.size+
+							'</div>').appendTo('#blockdata').remove('#block'+removeBlock);
+						}
+					})
+				}
+			})
+		},10000);
+		$.getJSON("/blocks/getblockdata", function(Data){
+			$(Data).each(function(){
+				$(
+					'<div id="block'+this.Block.height+'">'+this.Block.time+'　'+
+					'<a href="/blocks/detail/' + this.Block.height + '">' + this.Block.height + '</a>'+
+					'　'+this.Block.tx_count+
+					'　'+this.Block.size+
+				'</div>').appendTo('#blockdata');
+				})
+		})
 });
